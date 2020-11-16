47d32ca9dce63b853b6edd93625065cb4e3cb1e9
Saeid Javadi <SjavadiP@gmail.com>
2020-11-16 22:40:53 +0330

    add custom users

---
+++ account/managers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class myUserMnager(BaseUserManager):
+    def create_user(self, phone, full_name,password):
+        if not phone:
+            raise ValueError(_('users must have Phone Number'))
+        if not full_name:
+            raise ValueError(_('users must have full name'))
+
+        user = self.model(phone=self.phone,full_name=full_name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone, full_name,password):
+        user = self.create_user(phone, full_name, password)
+        user.is_admin =True
+        user.save(using=self._db)
+        return user
