b4c60d086f2efe2580500e4ccce9b4b91096b86d
Saeid Javadi <Yek1@Yahoo.com>
2020-11-28 13:58:19 +0330

    login |gw , logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/',views.LoginPage, name='login'),
     path('register/',views.RegisterPage, name='register'),
     path('verify/',views.VerifyPage, name='verify'),
+    path('logout/',views.LogoutPage, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
 from random import randint
-
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from accounts.forms import PhoneLoginForm, VerifyCodeForm, RegisterForm
@@ -8,22 +7,22 @@ from accounts.models import User
 from django.utils.translation import gettext_lazy as _
 
 
-
 def LoginPage(request):
     if request.method == 'POST':
-        global code , phone
+        global code, phone
         form = PhoneLoginForm(request.POST)
         if form.is_valid():
             phone = form.cleaned_data['phone']
             if User.objects.filter(phone=phone).exists():
+                request.session['phone'] = f"0{phone}"
                 return redirect('accounts:verify')
             else:
                 phone = f"0{phone}"
                 # code = str(randint(1000, 9999))
                 code = str(1234)
-                print('+'*10,'New Code','+'*10)
-                print(code)
-                print('+'*10,'New Code','+'*10)
+                # print('+'*10,'New Code','+'*10)
+                # print(code)
+                # print('+'*10,'New Code','+'*10)
                 # api = KavenegarAPI('54624B564154623558564355506C59417230747550612F7456524A544F4B733535374A624830485856456B3D')
                 # params = {'sender':'', 'receptor':phone, 'message':rand_num}
                 # api.sms_send(params)
@@ -48,7 +47,8 @@ def RegisterPage(request):
         if form.is_valid():
             password = form.cleaned_data['password']
             if password == code:
-                user = User.objects.create_user(phone=form.cleaned_data['phone'], full_name=form.cleaned_data['full_name'],
+                user = User.objects.create_user(phone=form.cleaned_data['phone'],
+                                                full_name=form.cleaned_data['full_name'],
                                                 password=password)
                 user.save()
                 return redirect('accounts:login')
@@ -61,9 +61,11 @@ def RegisterPage(request):
 def VerifyPage(request):
     if request.method == 'POST':
         form = VerifyCodeForm(request.POST)
+        phone = request.session['phone']
+        print(phone)
         if form.is_valid():
-            if code == form.cleaned_data['code']:
-                user = get_object_or_404(User, phone=phone, password=code)
+            user = authenticate(request, username=phone, password=form.cleaned_data['code'])
+            if user:
                 login(request, user)
                 messages.success(request, 'logged in successfully', 'success')
                 return redirect('memorial:home')
@@ -72,3 +74,9 @@ def VerifyPage(request):
     else:
         form = VerifyCodeForm()
     return render(request, 'accounts/verify.html', {'form': form})
+
+
+def LogoutPage(request):
+    logout(request)
+    messages.success(request, 'you logged out successfully', 'success')
+    return redirect('memorial:home')
