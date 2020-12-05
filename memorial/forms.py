7d02df3594848e4b2de965df6bab4b5800f65e00
Saeid Javadi <SjavadiP@gmail.com>
2020-12-05 18:07:34 +0330

    edit text

--- memorial/forms.py
+++ memorial/forms.py
@@ -4,4 +4,4 @@ from django.utils.translation import gettext_lazy as _
 searchD = {'class': 'form-control', 'placeholder': 'Search'}
 
 class Search(forms.Form):
-    search = forms.CharField(max_length=200, label=_(''), widget=forms.TextInput(attrs=searchD))
+    search = forms.CharField(max_length=200, label='', widget=forms.TextInput(attrs=searchD))
