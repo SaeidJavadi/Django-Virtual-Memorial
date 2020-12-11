f39876edd709ec46b24749c2ac6c7884f9154ac1
Saeid Javadi <SjavadiP@gmail.com>
2020-12-11 21:03:36 +0330

    start detail view

--- memorial/urls.py
+++ memorial/urls.py
@@ -9,4 +9,5 @@ urlpatterns= [
     path('edit/<int:id>/', views.DeveasedEdit, name='edit_Deveased'),
     path('list', views.list, name='list_Deveased'),
     path('delete/<int:id>/', views.delete, name='del_Deveased'),
+    path('<int:pk>/', views.DeadDetailView.as_view(), name='view_Deveased'),
 ]
