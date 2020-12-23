fb0f9457b683dbf8e94e5385d3c2503fc53ba386
Saeid Javadi <Yek1@Yahoo.com>
2020-12-23 10:36:41 +0330

    joz counter 90%

--- memorial/views.py
+++ memorial/views.py
@@ -135,68 +135,7 @@ def vote(request):
         ip = request.META['REMOTE_ADDR']
         marhom_id = request.POST['marhom_id']
         marhom = Deveased.objects.get(id=marhom_id)
-        try:
-            if request.POST['quran']:
-                quran = marhom.deadquran
-                jozs = {}
-                jozs['j1'] = quran.j1.count()
-                jozs['j2'] = quran.j2.count()
-                jozs['j3'] = quran.j3.count()
-                jozs['j4'] = quran.j4.count()
-                jozs['j5'] = quran.j5.count()
-                jozs['j6'] = quran.j6.count()
-                jozs['j7'] = quran.j7.count()
-                jozs['j8'] = quran.j8.count()
-                jozs['j9'] = quran.j9.count()
-                jozs['j10'] = quran.j10.count()
-                jozs['j11'] = quran.j11.count()
-                jozs['j12'] = quran.j12.count()
-                jozs['j13'] = quran.j13.count()
-                jozs['j14'] = quran.j14.count()
-                jozs['j15'] = quran.j15.count()
-                jozs['j16'] = quran.j16.count()
-                jozs['j17'] = quran.j17.count()
-                jozs['j18'] = quran.j18.count()
-                jozs['j19'] = quran.j19.count()
-                jozs['j20'] = quran.j20.count()
-                jozs['j21'] = quran.j21.count()
-                jozs['j22'] = quran.j22.count()
-                jozs['j23'] = quran.j23.count()
-                jozs['j24'] = quran.j24.count()
-                jozs['j25'] = quran.j25.count()
-                jozs['j26'] = quran.j26.count()
-                jozs['j27'] = quran.j27.count()
-                jozs['j28'] = quran.j28.count()
-                jozs['j29'] = quran.j29.count()
-                jozs['j30'] = quran.j30.count()
-                # sorted(jozs.values(), reverse=True)
 
-                # print('max',sorted(jozs.values(), reverse=True)[:1])
-                # print('min',sorted(jozs.values())[:1])
-                max_key = max(jozs, key=jozs.get)
-                print(max_key)
-                # print('min',jozs[min])
-                # jMax = 0
-                # jMin = 0
-                # jozMin = None
-                # for j in jozs:
-                #     print(f"{jMin} < {jozs[j]} < {jMax}")
-                #     if jozs[j] > jMax :
-                #         jMax = jozs[j]
-                #     else:
-                #         if jozs[j] > 0 :
-                #             jMin = jozs[j]
-                #             jozMin = j
-
-
-                #
-                # print('Khatm Count:',jMin)
-                # print(jozMin)
-                # print('JozMax',jMax)
-
-
-        except:
-            print('Error!')
         if request.POST['btn'] == 'f':
             vote = Fatehe.objects.create(dead=marhom, ip=ip)
             count = Fatehe.objects.all().filter(dead=marhom).count()
@@ -330,6 +269,49 @@ def vote(request):
             vote = Joz30.objects.create(quran=khtmQuran, ip=ip)
             count = Joz30.objects.all().filter(quran=khtmQuran).count()
 
+
+        try:
+            if request.POST['quran']:
+                quran = marhom.deadquran
+                jozs = {}
+                jozs['j1'] = quran.j1.count()
+                jozs['j2'] = quran.j2.count()
+                jozs['j3'] = quran.j3.count()
+                jozs['j4'] = quran.j4.count()
+                jozs['j5'] = quran.j5.count()
+                jozs['j6'] = quran.j6.count()
+                jozs['j7'] = quran.j7.count()
+                jozs['j8'] = quran.j8.count()
+                jozs['j9'] = quran.j9.count()
+                jozs['j10'] = quran.j10.count()
+                jozs['j11'] = quran.j11.count()
+                jozs['j12'] = quran.j12.count()
+                jozs['j13'] = quran.j13.count()
+                jozs['j14'] = quran.j14.count()
+                jozs['j15'] = quran.j15.count()
+                jozs['j16'] = quran.j16.count()
+                jozs['j17'] = quran.j17.count()
+                jozs['j18'] = quran.j18.count()
+                jozs['j19'] = quran.j19.count()
+                jozs['j20'] = quran.j20.count()
+                jozs['j21'] = quran.j21.count()
+                jozs['j22'] = quran.j22.count()
+                jozs['j23'] = quran.j23.count()
+                jozs['j24'] = quran.j24.count()
+                jozs['j25'] = quran.j25.count()
+                jozs['j26'] = quran.j26.count()
+                jozs['j27'] = quran.j27.count()
+                jozs['j28'] = quran.j28.count()
+                jozs['j29'] = quran.j29.count()
+                jozs['j30'] = quran.j30.count()
+                # max_j = sorted(jozs, key=(lambda key:jozs[key]), reverse=True)
+                min_j = sorted(jozs, key=(lambda key:jozs[key]))
+                print(min_j)
+                offer = min_j[0]
+                KhatmCount = jozs.get(offer)
+                print(KhatmCount)
+        except:
+            print('Error!')
         if vote:
                 response = {
                     'status': 'ok',
