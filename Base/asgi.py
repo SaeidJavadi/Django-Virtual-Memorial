ee7a3d97a608915be1b3d758aa6416f95d1c004d
Saeid Javadi <sjavadip@gmail.com>
2022-09-19 13:37:37 +0430

    chnm

--- Base/asgi.py
+++ Base/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Islamic project.
+ASGI config for Base project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Islamic.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Base.settings')
 
 application = get_asgi_application()
